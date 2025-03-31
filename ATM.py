3b14ef4a6cf95c6e4ea6a168a47f254990a35dbd
saikiran01415 <pvkc22@gmail.com>
2025-03-31 11:56:51 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,14 @@
+Bal=int(input("Enter Bal Amount:")
+if tran is 'w|W'
+   print("Withdrawl")
+elif tran is 'd|D'
+   print("Deposit")
+tamnt=int(input("Enter amount:")
+if typ is 'w|W'
+   print('tamnt>obal',tamnt)
+   print("ins.Fund")
+elif typ is 'd|D'
+    print('tamnt>=25000',tamnt)
+    print('Req pan')
+else
+    print("invalid Choice")
