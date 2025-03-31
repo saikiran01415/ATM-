20bcef0fe88121d64d7a22e306f4bb8849625e24
saikiran01415 <pvkc22@gmail.com>
2025-03-31 12:10:14 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,14 +1,25 @@
-Bal=int(input("Enter Bal Amount:")
-if tran is 'w|W'
-   print("Withdrawl")
-elif tran is 'd|D'
-   print("Deposit")
-tamnt=int(input("Enter amount:")
-if typ is 'w|W'
-   print('tamnt>obal',tamnt)
-   print("ins.Fund")
-elif typ is 'd|D'
-    print('tamnt>=25000',tamnt)
-    print('Req pan')
-else
-    print("invalid Choice")
+oBal=int(input("Enter old Bal Amount:"))
+ttype = int("Enter w for withdraw and d for deposit")
+tamnt= int(input("Enter transaction amount:")
+           if ttype=="w" or ttype=="W":
+             if tamnt>old:
+                 print("insufficient funds")
+           elif tamnt==obal:
+                 print("Pls maintain Min bal")
+         else:
+             cbal=obal-tamnt
+             print("your current bal=",cbal)
+elif ttype=="d" or ttype=="d":
+      if tamnt>=50000:
+         print("required pan")
+      pan=input("do you have pan type y for yes=")
+      if pan=="y" or pan=="y":
+         cbal=obal+tamnt
+      print("current bal=",cbal)
+     else:
+      print("invalid input")
+     else:
+        cbal=obal+tant
+      print("current balance=",cbal)
+      
+  
