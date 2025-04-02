437205f3ff60075337191fef102dac65b64a0921
saikiran01415 <pvkc22@gmail.com>
2025-04-02 07:12:39 +0530

    Update ATM.py
    added time module for better experence

--- ATM.py
+++ ATM.py
@@ -1,25 +1,43 @@
-oBal=int(input("Enter old Bal Amount:"))
-ttype = int("Enter w for withdraw and d for deposit")
-tamnt= int(input("Enter transaction amount:")
-           if ttype=="w" or ttype=="W":
-             if tamnt>old:
-                 print("insufficient funds")
-           elif tamnt==obal:
-                 print("Pls maintain Min bal")
+import time
+print("welcome to ATM")
+print("="*30)
+print("pls insert ATM card")
+time.sleep(2)
+print("pls wait for 3 sec")
+time.sleep(3)
+print("="*30)
+print("1.Deposit \n2.Withdraw \n3.exit")
+try:
+   inp=int(input("pls enter your option="))
+except ValueError:
+   print("input must be 1 or 2 or 3 try again")
+   time.sleep(3)
+   exit()
+try:
+   oldbal=int(input("pls enter your old balance="))
+   amount=int(input("pls enter the (W/D) amount="))
+except ValueError:
+   print(" amount should be in in numbers only")
+match inp:
+   case 1:
+      if amount>=50000:
+         panin=input("for depo above 50000 you need pan\ndo you have pan (y/n)=")
+         if panin in "Yy":
+            print("garte!.........")
+            print("current balence=",oldbal+amount)
          else:
-             cbal=obal-tamnt
-             print("your current bal=",cbal)
-elif ttype=="d" or ttype=="d":
-      if tamnt>=50000:
-         print("required pan")
-      pan=input("do you have pan type y for yes=")
-      if pan=="y" or pan=="y":
-         cbal=obal+tamnt
-      print("current bal=",cbal)
-     else:
-      print("invalid input")
-     else:
-        cbal=obal+tant
-      print("current balance=",cbal)
-      
-  
+            print("sorry with out pan we cant prossed!........")
+   case 2:
+      if oldbal<amount:
+         print("insuffecient funds!")
+      elif oldbal==amount:
+         print("sorry min bal should be maintained")
+      else:
+         currentbal=oldbal-amount
+         print("current bal=",currentbal)
+   case 3:
+      exit()
+
+   
+
+            
